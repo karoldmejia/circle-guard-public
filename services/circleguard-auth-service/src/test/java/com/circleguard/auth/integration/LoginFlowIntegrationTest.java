bafccb7ea2cc3d2bb7b62ad35e6aeb326bc01576
karoldmejia <karoldmejiaoz@gmail.com>
2026-05-11 17:15:11 -0500

    update jenkins identity

--- services/circleguard-auth-service/src/test/java/com/circleguard/auth/integration/LoginFlowIntegrationTest.java
+++ services/circleguard-auth-service/src/test/java/com/circleguard/auth/integration/LoginFlowIntegrationTest.java
@@ -31,7 +31,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @Testcontainers
 @ActiveProfiles("integration")
-@Disabled
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class LoginFlowIntegrationTest {
 
